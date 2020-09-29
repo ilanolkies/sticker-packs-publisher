365fc28b050bd835aea1aacc1b9f1f474980114b
Ilan Olkies <ilanolkies@outlook.com>
2020-09-29 14:54:51 -0300

    Add style for titles

--- src/pages/NewStickerPack.tsx
+++ src/pages/NewStickerPack.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 export default () => <div className="new-sticker-pack-page">
-  <h2>New sticker pack</h2>
-  <h3>Metadata</h3>
-  <h3>Stickers</h3>
-  <h3>Sticker Market Banner</h3>
-  <h3>Stickers\ Pack Thumbnail</h3>
+  <div className="new-sticker-pack-page-container">
+    <h2>New sticker pack</h2>
+    <h3>Metadata</h3>
+    <h3>Stickers</h3>
+    <h3>Sticker Market Banner</h3>
+    <h3>Stickers\ Pack Thumbnail</h3>
+  </div>
 </div>
